84cad06ac71127dd46d1a42f30acf9c37ee50b22
Kri4evskiy <kri4evskiy@gmail.com>
2020-11-08 18:41:25 +0200

    Fixed small bug

--- React-theory/react-pizza/src/components/Categories.jsx
+++ React-theory/react-pizza/src/components/Categories.jsx
@@ -10,16 +10,22 @@ export default function Categories({ items }) {
     return (
         <div className='categories'>
             <ul>
-                <li className='active'>Все</li>
-                {items.map((item, index) => (
-                    <li
-                        className={activeItem === index ? 'active' : ''}
-                        onClick={() => onSelectItem(index)}
-                        key={`${item}_${index}`}
-                    >
-                        {item}
-                    </li>
-                ))}
+                <li
+                    className={activeItem === null ? 'active' : ''}
+                    onClick={() => onSelectItem(null)}
+                >
+                    Все
+                </li>
+                {items &&
+                    items.map((item, index) => (
+                        <li
+                            className={activeItem === index ? 'active' : ''}
+                            onClick={() => onSelectItem(index)}
+                            key={`${item}_${index}`}
+                        >
+                            {item}
+                        </li>
+                    ))}
             </ul>
         </div>
     )
