2cf78ac98026f4ed3287f40bb946395a02ce519c
Kri4evskiy <kri4evskiy@gmail.com>
2020-11-11 10:06:37 +0200

    Never stop

--- React-theory/react-pizza/src/components/index.js
+++ React-theory/react-pizza/src/components/index.js
@@ -1,9 +1,11 @@
 import Header from './Header'
 import Button from './Button'
 import Categories from './Categories'
+import SortPopup from './SortPopup'
 
 export {
     Header,
     Button,
-    Categories
+    Categories,
+    SortPopup
 }
