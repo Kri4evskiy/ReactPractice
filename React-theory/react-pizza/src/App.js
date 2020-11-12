dadf808388acdf607e42918e2b5e41e96e303aac
Kri4evskiy <kri4evskiy@gmail.com>
2020-11-12 15:56:01 +0200

    Next implementing redux

--- React-theory/react-pizza/src/App.js
+++ React-theory/react-pizza/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import {Redirect, Route} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Redirect, Route } from 'react-router-dom'
+import axios from 'axios'
 
 import { Header } from './components'
-import {Home , Cart} from './pages'
+import { Home, Cart } from './pages'
 
 
 function App() {
+
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({data}) => {
+      setPizzas(data.pizzas)
+    })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' render={() => <Home items={pizzas} />} />
         <Route path='/cart' component={Cart} />
         <Redirect to='/' />
       </div>
--- React-theory/react-pizza/src/components/index.js
+++ React-theory/react-pizza/src/components/index.js
@@ -2,10 +2,12 @@ import Header from './Header'
 import Button from './Button'
 import Categories from './Categories'
 import SortPopup from './SortPopup'
+import PizzaBlock from './PizzaBlock'
 
 export {
     Header,
     Button,
     Categories,
-    SortPopup
+    SortPopup,
+    PizzaBlock
 }
